7f031ef4d69a32d4c9423e942a5c49ada45eb4b8
Y.Young <yyoung2001@gmail.com>
2017-07-26 20:28:07 +0800

    新增 Validation使用自定义的错误信息 新增 File,Report Model

---
+++ app/File.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    protected $table = 'files';
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected function buildFailedValidationResponse(Request $request, array $errors) {
+        return ['error' => 1 , 'msg' => current($errors)[0]];
+    }
 }
