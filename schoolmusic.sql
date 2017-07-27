7f8bacc129bfa356623ffd9208b2fbbd8dfb8f34
Y.Young <yyoung2001@gmail.com>
2017-07-27 21:14:28 +0800

    修改 数据库结构导出

--- schoolmusic.sql
+++ schoolmusic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017-07-24 07:47:56
+-- Generation Time: 2017-07-27 15:13:52
 -- 服务器版本： 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -19,6 +19,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `schoolmusic`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `files`
+--
+
+CREATE TABLE `files` (
+  `id` smallint(9) NOT NULL COMMENT 'ID',
+  `md5` tinytext NOT NULL COMMENT '文件md5',
+  `uploader` varchar(11) NOT NULL COMMENT '上传者学号',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- 转存表中的数据 `files`
+--
+
+INSERT INTO `files` (`id`, `md5`, `uploader`, `time`) VALUES
+(1, '***REMOVED***', '0', '2017-07-27 12:40:49');
 
 -- --------------------------------------------------------
 
@@ -45,11 +65,18 @@ CREATE TABLE `songs` (
   `playtime` tinyint(1) NOT NULL COMMENT '播放时段',
   `name` tinytext NOT NULL COMMENT '曲名',
   `origin` tinytext COMMENT '来源',
-  `uploader` varchar(11) NOT NULL COMMENT '上传者学籍号',
-  `url` tinytext NOT NULL COMMENT '文件地址',
-  `md5` tinytext NOT NULL COMMENT '文件md5值',
+  `uploader` varchar(11) NOT NULL COMMENT '上传者学号',
+  `file` smallint(6) NOT NULL COMMENT '文件ID',
   `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `songs`
+--
+
+INSERT INTO `songs` (`id`, `playtime`, `name`, `origin`, `uploader`, `file`, `time`) VALUES
+(1, 1, '***REMOVED***', '***REMOVED***', '0', 1, '2017-07-27 12:40:49'),
+(2, 2, '***REMOVED***', '***REMOVED***', '0', 1, '2017-07-27 13:00:58');
 
 -- --------------------------------------------------------
 
@@ -70,6 +97,12 @@ CREATE TABLE `vote` (
 --
 
 --
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `reports`
 --
 ALTER TABLE `reports`
@@ -80,6 +113,7 @@ ALTER TABLE `reports`
 --
 ALTER TABLE `songs`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
   ADD KEY `ref_time` (`uploader`,`time`);
 
 --
@@ -93,10 +127,20 @@ ALTER TABLE `vote`
 --
 
 --
+-- 使用表AUTO_INCREMENT `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` smallint(9) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=2;
+--
 -- 使用表AUTO_INCREMENT `reports`
 --
 ALTER TABLE `reports`
   MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT COMMENT 'ID';
+--
+-- 使用表AUTO_INCREMENT `songs`
+--
+ALTER TABLE `songs`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=3;
 --
 -- 使用表AUTO_INCREMENT `vote`
 --
