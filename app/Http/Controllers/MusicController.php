cf1bd94e911e37d64b398c828995b257c9c7cfc4
Y.Young <yyoung2001@gmail.com>
2017-07-30 22:28:30 +0800

    新增 MusicController历年校园音乐歌单 修改 MusicController使用Validator 修复 MusicController获取MP3找不到时返回为空的Bug

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Response;
-use Illuminate\Support\Facades\Config;
+
+use Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 use Metowolf\Meting;
 
 class MusicController extends Controller
@@ -11,21 +12,41 @@ class MusicController extends Controller
 
     public static $API;
 
-    public function _construct() {
-        
+    public function __construct() {
+        self::$API = new Meting('netease');
     }
 
-    public function Search(Request $req) {
-        if($req->has('keyword')) {
-            self::$API = new Meting('netease');
-            return self::$API->format(true)->search($req->input('keyword'));
-        }
+    public function Search(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'keyword' => 'required'
+        ], ['required' => '请输入搜索词']);
+        if($validator->fails())
+            return response()->json(['error' => 1, 'msg' => $validator->errors()->first()]);
+        return self::$API->format(true)->search($request->input('keyword'));
     }
 
-    public function Mp3(Request $req) {
-        if($req->has('id')) {
-            self::$API = new Meting('netease');
-            return json_decode(self::$API->format(true)->url($req->input('id')))->url;
-        }
+    public function Mp3(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required'
+        ], ['required' => '参数错误,请刷新重试']);
+        if($validator->fails())
+            return response()->json(['error' => 1, 'msg' => $validator->errors()->first()]);
+        $url = json_decode(self::$API->format(true)->url($request->input('id')))->url;
+        if(empty($url))
+            return response()->json(['error' => 1, 'msg' => '歌曲未找到']);
+        return $url;
+    }
+
+    public function Playlist() {
+        $res = json_decode(self::$API->format(true)->playlist(163809839), true);
+        $res = array_map(function($song) {
+            return [
+                'title' => $song['name'],
+                'author' => implode('', $song['artist']),
+                'url' => json_decode(self::$API->format(true)->url($song['id']))->url,
+                'pic' => json_decode(self::$API->format(true)->pic($song['pic_id']))->url
+            ];
+        }, $res);
+        return json_encode($res);
     }
 }
