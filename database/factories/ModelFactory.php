d4b64d82b6e096a75e0391979c95c699e212692c
Y.Young <yyoung2001@gmail.com>
2017-08-09 18:43:17 +0800

    Add Factories Add TestCase

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,7 +13,20 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'stuId' => rand(315, 317).'01010'.rand(100,800)
+    ];
+});
+$factory->define(App\Song::class, function(Faker\Generator $faker) use ($factory) {
+    return [
+        'playtime' => $faker->numberBetween(1, 6),
+	'name' => $faker->words(rand(1, 4), true),
+	'origin' => $faker->words(rand(1, 3), true),
+	'uploader' => $factory->raw('App\User')['stuId']	
+    ];
+});
+$factory->define(App\File::class, function (Faker\Generator $faker) use ($factory) {
+    return [
+	'md5' => $faker->md5,
+	'uploader' => $factory->raw('App\User')['stuId']
     ];
 });
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,13 +5,60 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    public function testPages()
     {
-        $this->assertTrue(true);
+        $response = $this->call('GET', '/');
+	$this->assertEquals(200, $response->status());
+	$response = $this->call('GET', '/Login');
+	$this->assertEquals(200, $response->status());
+	$response = $this->call('GET', '/Manage');
+	$this->assertEquals(404, $response->status());
+    }
+
+    public function testLogin()
+    {
+        $this->json('POST', '/Login')
+	     ->seeJson([
+		 'error' => 1
+	     ]);
+	$this->json('POST', '/Login', ['stuId' => '31601010'])
+	     ->seeJson([
+		 'error' => 1
+	     ]);
+    }
+
+    public function testAdmin()
+    {
+	$admin = factory('App\User')->make(['stuId' => '***REMOVED***']);
+	$response = $this->actingAs($admin)
+	                 ->call('GET', '/Manage');
+	$this->assertEquals(200, $response->status());
+	$this->actingAs($admin)
+	     ->json('GET', '/Manage/Songs')
+	     ->seeJson([
+		 'error' => 0
+	     ]);
+        $this->actingAs($admin)
+	     ->json('GET', '/Manage/Files')
+	     ->seeJson([
+		 'error' => 0
+	     ]);
+	$this->actingAs($admin)
+	     ->json('GET', '/Manage/Votes')
+	     ->seeJson([
+		 'error' => 0
+	     ]);
+        $this->actingAs($admin)
+	     ->json('GET', '/Manage/Reports')
+	     ->seeJson([
+		 'error' => 0
+	     ]);
+        $this->actingAs($admin)
+	     ->json('GET', '/Manage/Rank')
+	     ->seeJson([
+		 'error' => 0
+	     ]);
     }
 }
