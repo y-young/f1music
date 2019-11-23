fc10136b2c77e3ec7b6b02fe9f1d5313ee09048e
Googleplex <yyoung2001@gmail.com>
2019-11-23 14:59:07 +0800

    Fix: Status datetime format

--- config/music.php
+++ config/music.php
@@ -9,13 +9,15 @@ return [
     'debugAuth' => env('APP_DEBUG', false),
     'playlist' => '2064024722',
     'status' => [
+        //Format: ISO 8601 / RFC 2822 Date time
+        //example: 1970-01-01 00:00
         'upload' => [
             'start' => '2019-11-21 00:00',
             'end' => '2019-12-08 00:00'
         ],
         'vote' => [
             'start' => '2019-12-22 00:00',
-            'end' => '2020-1-5 00:00'
+            'end' => '2020-01-05 00:00'
         ]
     ]
 ];
