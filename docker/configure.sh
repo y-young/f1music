c2f853c36c5931cbccd339e46a656b7c0e9faf49
yyoung <yyoung2001@gmail.com>
2023-03-22 15:54:48 +0800

    fix: view path not found error when building image

--- docker/configure.sh
+++ docker/configure.sh
@@ -4,16 +4,16 @@
 sed -i "0,/upload_max_filesize = 2M/s//upload_max_filesize = 20M/" "$PHP_INI_DIR/php.ini"
 sed -i "0,/post_max_size = 8M/s//post_max_size = 20M/" "$PHP_INI_DIR/php.ini"
 
-# Cache routes and templates
-php artisan route:cache
-php artisan view:cache
-
 # Setup storage directories
 mkdir -p storage/app/uploads
 mkdir -p storage/framework/cache/data
 mkdir -p storage/framework/sessions
 mkdir -p storage/framework/views
 chown -R www-data:www-data storage
+
+# Cache routes and templates
+php artisan route:cache
+php artisan view:cache
 
 # Link media file storage to public directory
 php artisan storage:link
