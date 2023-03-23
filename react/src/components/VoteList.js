a11c22a7eee75460b6303b38baeaf2cf1ea27605
yyoung <yyoung2001@gmail.com>
2023-03-23 11:13:23 +0800

    refactor: Vote & VoteList to functional component

--- react/src/components/VoteList.js
+++ react/src/components/VoteList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { connect } from "dva";
 import { Link } from "dva/router";
 import { Spin, Input, Rate, Button, message } from "antd";
@@ -8,362 +8,355 @@ import styles from "./VoteList.css";
 import YPlayer from "./YPlayer";
 import { voteTexts } from "config";
 
-class VoteList extends React.Component {
-  state = {
-    rate: 0,
-    src: "",
-    reason: "",
-    index: "",
-    canVote: false,
-    canSubmit: false,
-    showReport: false,
-    triggerVote: true,
-    countdown: 30,
-    canBackward: false,
-    canForward: false
+const VoteList = ({ vote, dispatch, loading, time, songs }) => {
+  const { onDesktop } = vote;
+  const playerRef = useRef(null);
+  const [rate, setRate] = useState(0);
+  const [src, setSrc] = useState("");
+  const [reason, setReason] = useState("");
+  const [index, setIndex] = useState("");
+  const [canSubmit, setCanSubmit] = useState(false);
+  const [showReport, setShowReport] = useState(false);
+  const [triggerVote, setTriggerVote] = useState(true);
+  const [countdown, setCountdown] = useState(30);
+  const [canBackward, setCanBackward] = useState(false);
+  const [canForward, setCanForward] = useState(false);
+
+  const init = () => {
+    setRate(0);
+    setCountdown(30);
+    setReason("");
+    setCanSubmit(false);
+    setShowReport(false);
+    setTriggerVote(true);
+    setCanBackward(false);
+    setCanForward(false);
   };
 
-  init = () => {
-    this.setState({
-      rate: 0,
-      countdown: 30,
-      reason: "",
-      canVote: false,
-      canSubmit: false,
-      showReport: false,
-      triggerVote: true,
-      canBackward: false,
-      canForward: false
-    });
-  };
-  stopLast = () => {
-    if (this.player) {
-      this.player.stop();
+  useEffect(() => {
+    stopLast();
+    setIndex("");
+    setSrc("");
+    init();
+  }, [time]);
+
+  useEffect(() => {
+    if (src) {
+      playerRef.current.play();
+    }
+  }, [src]);
+
+  useEffect(() => {
+    updateButtonStatus(index);
+  }, [index]);
+
+  const stopLast = () => {
+    if (playerRef.current) {
+      playerRef.current.stop();
     }
   };
-  onRedirect = () => {
-    this.stopLast();
-    this.setState({ index: "", src: "" });
-    this.init();
-  };
-  timeListener = offset => {
-    const { dispatch, vote } = this.props;
-    const { songs } = vote;
-    const song = songs[this.state.index];
-    if (this.state.countdown > 0) {
-      this.setState(prevState => {
-        return { countdown: prevState.countdown - offset };
-      });
+
+  const timeListener = offset => {
+    const song = songs[index];
+
+    if (countdown > 0) {
+      setCountdown(prevCountdown => prevCountdown - offset);
     } else {
       if (!song.listened && song.vote === 0) {
         dispatch({ type: "vote/markListened", payload: song.id });
       }
-      //triggerVote: Only when first listen
-      if (this.state.triggerVote) {
-        this.setState({ triggerVote: false });
-        this.handleVote();
+      // triggerVote: Only when first listen
+      if (triggerVote) {
+        setTriggerVote(false);
+        handleVote();
       }
     }
   };
-  handleSwitch = index => {
-    const { vote } = this.props;
-    const { songs } = vote;
-    const player = this.player;
-    if (index !== this.state.index) {
-      this.setState({ index: index }, () => {
-        this.updateButtonStatus();
-      });
-      this.stopLast();
-      this.init();
-      this.setState({ src: songs[index].url }, () => {
-        player.play();
-      });
-      if (songs[index].vote !== 0 || songs[index].listened) {
-        this.setState({
-          countdown: 0,
-          rate: songs[index].vote,
-          triggerVote: false
-        });
-      } else {
-        this.setState({ countdown: 30 });
-      }
-    } else {
+
+  const handleSwitch = newIndex => {
+    const player = playerRef.current;
+
+    if (index === newIndex) {
       player.toggle();
+      return index;
     }
-    return index;
+
+    setIndex(newIndex);
+    stopLast();
+    init();
+    setSrc(songs[newIndex].url);
+
+    if (songs[newIndex].vote !== 0 || songs[newIndex].listened) {
+      setCountdown(0);
+      setRate(songs[newIndex].vote);
+      setTriggerVote(false);
+    } else {
+      setCountdown(30);
+    }
+    return newIndex;
   };
-  updateButtonStatus = () => {
-    const { vote } = this.props;
-    const { songs } = vote;
-    const previous = Number(this.state.index) - 1;
-    const next = Number(this.state.index) + 1;
-    this.setState({
-      canBackward: songs[previous] !== undefined,
-      canForward: songs[next] !== undefined
-    });
+
+  const updateButtonStatus = index => {
+    const previous = Number(index) - 1;
+    const next = Number(index) + 1;
+    setCanBackward(songs[previous] !== undefined);
+    setCanForward(songs[next] !== undefined);
   };
-  forward = () => {
-    const { vote } = this.props;
-    const { songs, skipVoted } = vote;
-    let newIndex = Number(this.state.index) + 1;
+
+  const forward = () => {
+    const { skipVoted } = vote;
+    let newIndex = Number(index) + 1;
     if (skipVoted) {
       while (songs[newIndex] && songs[newIndex].vote !== 0) {
         newIndex++;
       }
     }
     if (songs[newIndex]) {
-      this.handleSwitch(newIndex);
+      handleSwitch(newIndex);
     } else if (skipVoted) {
       message.info("您已选择跳过已投票歌曲，如无需要请修改偏好设置");
     }
   };
-  backward = () => {
-    const { vote } = this.props;
-    const { songs, skipVoted } = vote;
-    let newIndex = Number(this.state.index) - 1;
+
+  const backward = () => {
+    const { skipVoted } = vote;
+    let newIndex = Number(index) - 1;
     if (skipVoted) {
       while (songs[newIndex] && songs[newIndex].vote !== 0) {
         newIndex--;
       }
     }
     if (songs[newIndex]) {
-      this.handleSwitch(newIndex);
+      handleSwitch(newIndex);
     } else if (skipVoted) {
       message.info("您已选择跳过已投票歌曲，如无需要请修改偏好设置");
     }
   };
-  checkValidity = () => {
-    if (this.state.countdown > 0) {
+
+  const checkValidity = () => {
+    if (countdown > 0) {
       message.warning("试听时长需达到30秒才能投票");
-      const player = this.player;
+      const player = playerRef.current;
       player.audio.currentTime = 0;
       player.play();
       return "time";
     }
-    if (!this.state.canSubmit || this.state.rate === 0) {
+    if (!canSubmit || rate === 0) {
       message.info("选择或更改评价后即可提交");
       return "rate";
     }
     return "valid";
   };
-  handleVote = (type = null) => {
-    const { dispatch, vote } = this.props;
-    const { songs, onSubmitted } = vote;
-    const song = songs[this.state.index];
-    const validity = this.checkValidity();
+
+  const handleVote = (type = null) => {
+    const { onSubmitted } = vote;
+    const song = songs[index];
+    const validity = checkValidity();
     if (validity !== "valid") {
       return;
     }
     const id = song.id;
-    const rate = this.state.rate;
     dispatch({ type: "vote/vote", payload: { id, rate } }).then(success => {
       if (success) {
         message.success("投票成功");
-        this.setState({ canSubmit: false });
+        setCanSubmit(false);
         if (
           (type === "manual" && onSubmitted === "forward") ||
           type === "ended"
         ) {
-          this.forward();
+          forward();
         }
       }
     });
   };
-  onEnded = () => {
-    const { vote } = this.props;
-    const { songs, onEnded } = vote;
-    const song = songs[this.state.index];
-    const validity = this.checkValidity();
+
+  const onEnded = () => {
+    const { onEnded } = vote;
+    const song = songs[index];
+    const validity = checkValidity();
     if (song.vote === 0) {
       if (validity === "valid") {
-        this.handleVote("ended");
+        handleVote("ended");
       } else if (validity === "rate" && onEnded === "forward") {
-        this.forward();
+        forward();
       }
     } else {
-      this.forward();
+      forward();
     }
   };
-  handleReport = () => {
-    const { dispatch, vote } = this.props;
-    const { songs } = vote;
-    const id = songs[this.state.index].id;
+
+  const submitReport = () => {
+    const id = songs[index].id;
     dispatch({
       type: "vote/report",
-      payload: { id: id, reason: this.state.reason }
+      payload: { id: id, reason: reason }
     }).then(success => {
       if (success) {
-        this.setState({ showReport: false });
+        setShowReport(false);
       }
     });
   };
-  render() {
-    const { vote, loading } = this.props;
-    const { time, isDesktop, songs } = vote;
-    const song = songs[this.state.index]
-      ? songs[this.state.index]
-      : { vote: 0 };
-    const buttonProps = {
-      type: song.vote !== 0 ? "secondary" : "primary",
-      shape: !isDesktop ? "circle" : undefined,
-      icon: this.state.countdown <= 0 ? <CheckOutlined /> : undefined,
-      disabled: this.state.countdown > 0
-    };
-    const voteArea = (
-      <div className={styles.voteArea} key="vote">
-        <hr />
-        {this.state.countdown <= 15 ? (
-          <div>
-            <Rate
-              value={this.state.rate}
-              onChange={value =>
-                this.setState({ rate: value, canSubmit: true })
-              }
-              allowClear={false}
-              className={styles.rate}
-            />
-            {this.state.rate !== 0 && (
-              <div className={styles.voteText}>
-                <span className="ant-rate-text">
-                  {voteTexts[this.state.rate]}
-                </span>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div className={styles.voteText}>
-            <span className="ant-rate-text" style={{ color: "#777" }}>
-              试听15秒后显示评分栏
-            </span>
-          </div>
-        )}
-        <Button
-          loading={loading.effects["vote/vote"]}
-          className={styles.voteButton}
-          onClick={() => this.handleVote("manual")}
-          {...buttonProps}
-        >
-          {this.state.countdown > 0
-            ? Math.ceil(this.state.countdown)
-            : isDesktop && "投票"}
-        </Button>
-      </div>
-    );
-    const reportArea = (
-      <div className={styles.reportArea} key="report">
-        <div className={styles.reason}>
-          <Input
-            value={this.state.reason}
-            placeholder="反馈内容"
-            onChange={e => this.setState({ reason: e.target.value })}
-            maxLength={200}
-            onPressEnter={this.handleReport}
+
+  const handleRatingChange = value => {
+    setRate(value);
+    setCanSubmit(true);
+  };
+
+  const song = songs[index] ? songs[index] : { vote: 0 };
+  const buttonProps = {
+    type: song.vote !== 0 ? "secondary" : "primary",
+    shape: !onDesktop ? "circle" : undefined,
+    icon: countdown <= 0 ? <CheckOutlined /> : undefined,
+    disabled: countdown > 0
+  };
+
+  const voteArea = (
+    <div className={styles.voteArea} key="vote">
+      <hr />
+      {countdown <= 15 ? (
+        <div>
+          <Rate
+            value={rate}
+            onChange={handleRatingChange}
+            allowClear={false}
+            className={styles.rate}
           />
-        </div>
-        <Button
-          type="primary"
-          onClick={this.handleReport}
-          loading={loading.effects["vote/report"]}
-          className={styles.reportButton}
-        >
-          提交
-        </Button>
-      </div>
-    );
-    const list = songs.map((song, key) => {
-      const current = key === this.state.index;
-      return (
-        <li
-          style={current ? { color: "#1890ff" } : {}}
-          className={styles.listItem}
-          onClick={() => this.handleSwitch(key)}
-          key={key}
-        >
-          <span className={styles.itemIndex}>{key + 1}</span>
-          {"您的评价: " + voteTexts[song.vote]}
-        </li>
-      );
-    });
-    const notice = () => {
-      let voted = 0;
-      songs.forEach(song => {
-        if (song.vote !== 0) {
-          voted++;
-        }
-      });
-      if (voted === songs.length) {
-        let rndTime;
-        do {
-          rndTime = Math.floor(Math.random() * 5 + 1);
-        } while (rndTime === time);
-        return (
-          <div className="tips">
-            <BulbOutlined /> 您已投完本时段所有曲目，到
-            <Link to={"/vote/" + rndTime}>其他时段</Link>
-            看看吧
-          </div>
-        );
-      } else {
-        return (
-          <div className="tips">
-            <BulbOutlined /> 本时段您已投 {voted} 首曲目，还有{" "}
-            {songs.length - voted} 首未投票曲目
-          </div>
-        );
-      }
-    };
-
-    return (
-      <Spin spinning={loading.effects["vote/fetchList"]}>
-        {songs.length !== 0 ? (
-          <span>
-            <div>
-              <YPlayer
-                src={this.state.src}
-                onProgress={this.timeListener}
-                onEnded={this.onEnded}
-                canBackward={this.state.canBackward}
-                canForward={this.state.canForward}
-                onBackward={this.backward}
-                onForward={this.forward}
-                ref={player => (this.player = player)}
-                className={styles.yplayer}
-              />
-              <br />
-              <Button
-                size="small"
-                onClick={() =>
-                  this.setState({ showReport: !this.state.showReport })
-                }
-                disabled={this.state.index === ""}
-                className={styles.toggleReport}
-              >
-                反馈
-              </Button>
+          {rate !== 0 && (
+            <div className={styles.voteText}>
+              <span className="ant-rate-text">{voteTexts[rate]}</span>
             </div>
-            <br />
-            {voteArea}
-            <TransitionGroup>
-              {this.state.showReport && (
-                <CSSTransition
-                  classNames="fade"
-                  timeout={{ enter: 500, exit: 200 }}
-                >
-                  {reportArea}
-                </CSSTransition>
-              )}
-            </TransitionGroup>
-            <ol className={styles.list}>{list}</ol>
-            {notice()}
+          )}
+        </div>
+      ) : (
+        <div className={styles.voteText}>
+          <span className="ant-rate-text" style={{ color: "#777" }}>
+            试听15秒后显示评分栏
           </span>
-        ) : (
-          <div className="tips">投票未开放或暂无数据</div>
-        )}
-      </Spin>
-    );
-  }
-}
+        </div>
+      )}
+      <Button
+        loading={loading.effects["vote/vote"]}
+        className={styles.voteButton}
+        onClick={() => handleVote("manual")}
+        {...buttonProps}
+      >
+        {countdown > 0 ? Math.ceil(countdown) : onDesktop && "投票"}
+      </Button>
+    </div>
+  );
 
-export default connect(({ vote, loading }) => ({ vote, loading }), null, null, {
-  withRef: true
-})(VoteList);
+  const reportArea = (
+    <div className={styles.reportArea} key="report">
+      <div className={styles.reason}>
+        <Input
+          value={reason}
+          placeholder="反馈内容"
+          onChange={e => setReason(e.target.value)}
+          maxLength={200}
+          onPressEnter={submitReport}
+        />
+      </div>
+      <Button
+        type="primary"
+        onClick={submitReport}
+        loading={loading.effects["vote/report"]}
+        className={styles.reportButton}
+      >
+        提交
+      </Button>
+    </div>
+  );
+
+  const list = songs.map((song, key) => {
+    const current = key === index;
+    return (
+      <li
+        style={current ? { color: "#1890ff" } : {}}
+        className={styles.listItem}
+        onClick={() => handleSwitch(key)}
+        key={key}
+      >
+        <span className={styles.itemIndex}>{key + 1}</span>
+        {"您的评价: " + voteTexts[song.vote]}
+      </li>
+    );
+  });
+
+  const notice = () => {
+    let voted = 0;
+    songs.forEach(song => {
+      if (song.vote !== 0) {
+        voted++;
+      }
+    });
+    if (voted === songs.length) {
+      let rndTime;
+      do {
+        rndTime = Math.floor(Math.random() * 5 + 1);
+      } while (rndTime === time);
+      return (
+        <div className="tips">
+          <BulbOutlined /> 您已投完本时段所有曲目，到
+          <Link to={"/vote/" + rndTime}>其他时段</Link>
+          看看吧
+        </div>
+      );
+    } else {
+      return (
+        <div className="tips">
+          <BulbOutlined /> 本时段您已投 {voted} 首曲目，还有{" "}
+          {songs.length - voted} 首未投票曲目
+        </div>
+      );
+    }
+  };
+
+  return (
+    <Spin spinning={loading.effects["vote/fetchList"]}>
+      {songs.length !== 0 ? (
+        <span>
+          <div>
+            <YPlayer
+              src={src}
+              onProgress={timeListener}
+              onEnded={onEnded}
+              canBackward={canBackward}
+              canForward={canForward}
+              onBackward={backward}
+              onForward={forward}
+              ref={playerRef}
+              className={styles.yplayer}
+            />
+            <br />
+            <Button
+              size="small"
+              onClick={() => setShowReport(showReport => !showReport)}
+              disabled={index === ""}
+              className={styles.toggleReport}
+            >
+              反馈
+            </Button>
+          </div>
+          <br />
+          {voteArea}
+          <TransitionGroup>
+            {showReport && (
+              <CSSTransition
+                classNames="fade"
+                timeout={{ enter: 500, exit: 200 }}
+              >
+                {reportArea}
+              </CSSTransition>
+            )}
+          </TransitionGroup>
+          <ol className={styles.list}>{list}</ol>
+          {notice()}
+        </span>
+      ) : (
+        <div className="tips">投票未开放或暂无数据</div>
+      )}
+    </Spin>
+  );
+};
+
+export default connect(({ vote, loading }) => ({ vote, loading }))(VoteList);
--- react/src/routes/Vote.js
+++ react/src/routes/Vote.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import { connect } from "dva";
 import { TimeSelector, VoteList } from "components";
@@ -8,131 +8,109 @@ import { PhaseCountdown } from "components";
 
 const FormItem = Form.Item;
 
-class Vote extends React.Component {
-  state = {
-    modalVisible: false
-  };
+const Vote = ({ vote, loading, dispatch }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const { time, skipVoted, onSubmitted, onEnded, status, songs } = vote;
 
-  componentDidUpdate(prevProps) {
-    if (
-      this.props.location.pathname !== prevProps.location.pathname &&
-      this.voteList
-    ) {
-      this.voteList.getWrappedInstance().onRedirect();
-    }
-  }
-
-  toggle = option => {
-    const { dispatch } = this.props;
+  const toggle = option => {
     dispatch({ type: "vote/toggle" + option });
   };
 
-  save = (option, value) => {
-    const { dispatch } = this.props;
+  const save = (option, value) => {
     dispatch({ type: "vote/save" + option, payload: value });
   };
 
-  handleRedirect = time => {
-    const { dispatch } = this.props;
-    this.voteList.getWrappedInstance().onRedirect();
+  const handleRedirect = time => {
     dispatch({ type: "vote/redirect", payload: time });
   };
 
-  render() {
-    const { vote, loading } = this.props;
-    const { time, skipVoted, onSubmitted, onEnded, status } = vote;
-
-    return (
-      <Spin spinning={loading.effects["vote/fetchStatus"]}>
-        {moment().isBefore(status.start) ? (
-          <Result
-            icon={<ClockCircleTwoTone />}
-            title="抱歉，投票尚未开始，距离投票开始还有"
-            subTitle={
-              <PhaseCountdown value={status.start} tooltipPlacement="bottom" />
+  return (
+    <Spin spinning={loading.effects["vote/fetchStatus"]}>
+      {moment().isBefore(status.start) ? (
+        <Result
+          icon={<ClockCircleTwoTone />}
+          title="抱歉，投票尚未开始，距离投票开始还有"
+          subTitle={
+            <PhaseCountdown value={status.start} tooltipPlacement="bottom" />
+          }
+          extra={
+            <Button type="primary" href="#/">
+              查看投票说明
+            </Button>
+          }
+        />
+      ) : moment().isAfter(status.end) ? (
+        <Result
+          status="error"
+          title="抱歉，投票已结束"
+          extra={
+            <Button type="primary" href="#/">
+              返回首页
+            </Button>
+          }
+        />
+      ) : (
+        <div>
+          <Alert
+            type="info"
+            closable
+            description={
+              <PhaseCountdown title="距离投票结束" value={status.end} />
             }
-            extra={
-              <Button type="primary" href="#/">
-                查看投票说明
-              </Button>
-            }
+            style={{ marginBottom: "10px" }}
           />
-        ) : moment().isAfter(status.end) ? (
-          <Result
-            status="error"
-            title="抱歉，投票已结束"
-            extra={
-              <Button type="primary" href="#/">
-                返回首页
-              </Button>
-            }
+          <TimeSelector
+            onChange={handleRedirect}
+            style={{ width: "90px", marginBottom: "10px" }}
+            value={time}
           />
-        ) : (
-          <div>
-            <Alert
-              type="info"
-              closable
-              description={
-                <PhaseCountdown title="距离投票结束" value={status.end} />
-              }
-              style={{ marginBottom: "10px" }}
-            />
-            <TimeSelector
-              onChange={this.handleRedirect}
-              style={{ width: "90px", marginBottom: "10px" }}
-              value={time}
-            />
-            <div style={{ float: "right" }}>
-              <Button
-                type="secondary"
-                onClick={() => this.setState({ modalVisible: true })}
-              >
-                <SettingOutlined />
-                偏好设置
-              </Button>
-              <Modal
-                title="偏好设置"
-                centered
-                open={this.state.modalVisible}
-                onCancel={() => this.setState({ modalVisible: false })}
-                footer={null}
-              >
-                <Form>
-                  <FormItem label="自动跳过已投票曲目">
-                    <Switch
-                      checked={skipVoted}
-                      onChange={() => this.toggle("SkipVoted")}
-                    />
-                  </FormItem>
-                  <FormItem label="手动提交成功后">
-                    <Radio.Group
-                      defaultValue={onSubmitted}
-                      onChange={e => this.save("OnSubmitted", e.target.value)}
-                      buttonStyle="solid"
-                    >
-                      <Radio.Button value="continue">继续播放</Radio.Button>
-                      <Radio.Button value="forward">切换下一首</Radio.Button>
-                    </Radio.Group>
-                  </FormItem>
-                  <FormItem label="播放结束但未投票时:">
-                    <Radio.Group
-                      defaultValue={onEnded}
-                      onChange={e => this.save("OnEnded", e.target.value)}
-                      buttonStyle="solid"
-                    >
-                      <Radio.Button value="pause">暂停</Radio.Button>
-                      <Radio.Button value="forward">播放下一首</Radio.Button>
-                    </Radio.Group>
-                  </FormItem>
-                </Form>
-              </Modal>
-            </div>
-            <VoteList ref={list => (this.voteList = list)} />
+          <div style={{ float: "right" }}>
+            <Button type="secondary" onClick={() => setModalVisible(true)}>
+              <SettingOutlined />
+              偏好设置
+            </Button>
+            <Modal
+              title="偏好设置"
+              centered
+              open={modalVisible}
+              onCancel={() => setModalVisible(false)}
+              footer={null}
+            >
+              <Form>
+                <FormItem label="自动跳过已投票曲目">
+                  <Switch
+                    checked={skipVoted}
+                    onChange={() => toggle("SkipVoted")}
+                  />
+                </FormItem>
+                <FormItem label="手动提交成功后">
+                  <Radio.Group
+                    defaultValue={onSubmitted}
+                    onChange={e => save("OnSubmitted", e.target.value)}
+                    buttonStyle="solid"
+                  >
+                    <Radio.Button value="continue">继续播放</Radio.Button>
+                    <Radio.Button value="forward">切换下一首</Radio.Button>
+                  </Radio.Group>
+                </FormItem>
+                <FormItem label="播放结束但未投票时:">
+                  <Radio.Group
+                    defaultValue={onEnded}
+                    onChange={e => save("OnEnded", e.target.value)}
+                    buttonStyle="solid"
+                  >
+                    <Radio.Button value="pause">暂停</Radio.Button>
+                    <Radio.Button value="forward">播放下一首</Radio.Button>
+                  </Radio.Group>
+                </FormItem>
+              </Form>
+            </Modal>
           </div>
-        )}
-      </Spin>
-    );
-  }
-}
+          <VoteList songs={songs} time={time} />
+        </div>
+      )}
+    </Spin>
+  );
+};
 
 export default connect(({ vote, loading }) => ({ vote, loading }))(Vote);
